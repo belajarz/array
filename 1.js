8ad9dc4f3529bdcea5c2d9b7eb31a4bc45df35f3
Alfian Horis <alfian.horis@merahciptamedia.co.id>
2023-02-24 23:04:55 +0700

    i

---
+++ 1.js
@@ -0,0 +1,14 @@
+// 1. Buatlah sebuah variabel dengan nama "animals"
+// 2. Isi variable tersebut dengan array yang memiliki 3 elemen string "kucing", "domba", "kuda"
+// 3. Cetak element array pertama ("kucing")
+// 4. Cetak element array ketiga ("kuda")
+// 5. Ganti element ketiga dari array menjadi "kelinci"
+// 6. Cetak element array ketiga ("kelinci")
+// 7. Gunakan property length untuk mencetak jumlah element di array
+// 8. Gunakan loop for untuk mencetak nilai animals di console secara berurutan
+// output:
+// kucing
+// domba
+// kuda
+
+
